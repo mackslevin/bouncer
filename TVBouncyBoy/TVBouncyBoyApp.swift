f670d728a5ff35a220396fe36dae730b2ec3efa1
Mack Slevin <mack.slevin@gmail.com>
2024-07-06 13:58:32 -0700

    Initial Commit

---
+++ TVBouncyBoy/TVBouncyBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVBouncyBoyApp.swift
+//  TVBouncyBoy
+//
+//  Created by Mack Slevin on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TVBouncyBoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
