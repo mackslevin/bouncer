af688ba0a1f01ba3ca96e9e567f656eb948073fd
Mack Slevin <mack.slevin@gmail.com>
2024-07-19 13:11:17 -0700

    Re-write tv app to use Canvas for performance

--- TVBouncyBoy/BounceView.swift
+++ TVBouncyBoy/BounceView.swift
@@ -18,24 +18,69 @@ struct BounceView: View {
         Button {
             vm.isShowingOptions.toggle()
         } label: {
-            RoundedRectangle(cornerRadius: 3)
-                .foregroundStyle(.gray.gradient)
-                .overlay(alignment: .center) {
-                    if let image = boxImageProxy {
-                        image
-                            .resizable().scaledToFill()
+//            RoundedRectangle(cornerRadius: 3)
+//                .foregroundStyle(.gray.gradient)
+//                .overlay(alignment: .center) {
+//                    if let image = boxImageProxy {
+//                        image
+//                            .resizable().scaledToFill()
+//                    }
+//                }
+//                .clipped()
+//                .frame(width: vm.rectangleSize.width, height: vm.rectangleSize.height)
+//                .position(vm.position)
+//                .onAppear {
+//                    vm.startTimer()
+//                    UIApplication.shared.isIdleTimerDisabled = true
+//                }
+//                .onDisappear {
+//                    vm.timer?.invalidate()
+//                }
+            
+            Canvas { context, size in
+                
+                if let bg = context.resolveSymbol(id: 0) {
+                    context.draw(bg, in: CGRect(origin: .zero, size: size))
+                }
+                
+                
+                let rect = CGRect(x: vm.position.x - vm.rectangleSize.width / 2,
+                                  y: vm.position.y - vm.rectangleSize.height / 2,
+                                  width: vm.rectangleSize.width,
+                                  height: vm.rectangleSize.height)
+                
+                if let boxy = context.resolveSymbol(id: 1) {
+                    context.draw(boxy, in: rect)
+                }
+            } symbols: {
+                Rectangle()
+                    .foregroundStyle(.gray)
+                    .overlay {
+                        if let bg = backgroundImageProxy {
+                            bg.resizable().scaledToFill()
+                        }
                     }
-                }
-                .clipped()
-                .frame(width: vm.rectangleSize.width, height: vm.rectangleSize.height)
-                .position(vm.position)
-                .onAppear {
-                    vm.startTimer()
-                    UIApplication.shared.isIdleTimerDisabled = true
-                }
-                .onDisappear {
-                    vm.timer?.invalidate()
-                }
+                    .clipped()
+                    .tag(0)
+                
+                RoundedRectangle(cornerRadius: Utility.defaultCornerRadius)
+                    .overlay {
+                        if let boxImg = boxImageProxy {
+                            boxImg.resizable().scaledToFill()
+                        }
+                    }
+                    .clipped()
+                    .frame(width: vm.rectangleSize.width, height: vm.rectangleSize.height)
+                    .tag(1)
+            }
+            .onAppear {
+                vm.startTimer()
+                UIApplication.shared.isIdleTimerDisabled = true
+            }
+            .onDisappear {
+                vm.timer?.invalidate()
+            }
+
         }
         .buttonStyle(BlankButtonStyle())
         .background {
